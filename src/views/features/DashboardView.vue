7bab53a25c491e16f6f3ddf710058f755681d293
Yogona <yonagodwin@gmail.com>
2023-06-22 15:29:32 +0300

    dashboard

--- src/views/features/DashboardView.vue
+++ src/views/features/DashboardView.vue
@@ -2,6 +2,8 @@
 // import { RouterView, RouterLink } from 'vue-router';
 import Nav from '../../components/Nav.vue';
 import TopBar from "../../components/TopBar.vue";
+import Progress from '../../components/Progress.vue';
+import { BIconPeople, BIconBuilding, BIconWindowStack } from 'bootstrap-icons-vue';
 
 export default {
   props: {
@@ -10,15 +12,38 @@ export default {
   },
   emits: ['locationChange'],
   components: {
-    Nav, TopBar
+    Nav, TopBar, Progress, BIconPeople, BIconBuilding, BIconWindowStack
   },
   data() {
     return {
-
+      statistics: null,
     };
   },
-  mounted(){
-    this.$emit('locationChange', location.pathname);
+  methods: {
+    async getStatistics() {
+      // this.isLoading = true;
+
+      await this.axios.get(this.api + "/statistics").then((res) => {
+        if (res.status == 200) {
+          this.statistics = res.data.data;
+          // let counter = 0;
+          // this.departments.forEach((depart) => {
+          //   depart.counter = ++counter;
+          // })
+        }
+      }).catch((err) => {
+        const resData = err.response.data;
+        this.title = resData.message;
+        this.message = this.title;
+      }).finally(() => {
+        this.isLoading = false;
+      });
+    }
+  },
+  async mounted(){
+    // this.$emit('locationChange', location.pathname);
+    await this.getStatistics();
+    console.log(this.user)
   }
 }
 </script>
@@ -28,11 +53,50 @@ export default {
   <div class="row" style="width: 100%;">
     <Nav  class="col-md-2" :user="user" :active="active" />
     <main class="col">
+      <Progress class="text-center" v-if="isLoading" message="Retrieving statistics." />
+      <h2 class="p-5" v-else-if="statistics == null">
+          {{ message }}
+      </h2>
+      <h2 class="p-5" v-else-if="statistics.length == 0">
+          {{ message }}
+      </h2>
+      <div v-else class="p-3 container overflow-hidden text-center">
+          <div class="row gy-5 justify-content-evenly pt-5">
+            <div v-if="user.role_id == 1" class="container-bg white-text col-md-4 summary">
+              <div class="row justify-content-center text-center">
+                <BIconPeople />
+                <span>{{ statistics.users }} Users</span>
+              </div>
+            </div>
 
+            <div v-if="user.role_id == 1" class="container-bg white-text col-md-4 summary">
+              <div class="row justify-content-center text-center">
+                <BIconBuilding />
+                <span>{{ statistics.departs }} Departments</span>
+              </div>
+            </div>
+
+            <div class="container-bg white-text col-md-4 summary">
+              <div class="row justify-content-center text-center">
+                <BIconBuilding />
+                <span>{{ statistics.stores }} Stores</span>
+              </div>
+            </div>
+
+            <div class="container-bg white-text col-md-4 summary">
+              <div class="row justify-content-center text-center">
+                <BIconWindowStack />
+                <span>{{ statistics.stores }} Instruments</span>
+              </div>
+            </div>
+          </div>
+      </div>
     </main>
   </div>
 </template>
 
 <style>
-
+.summary{
+  padding: 2em;
+}
 </style>
